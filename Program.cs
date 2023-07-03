a759c4448fc6b53f019a20d2f5156245f6804c4c
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-07-03 11:27:59 +0200

    Updated variables
    Updated variables

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            byte var1 = 1;//Unsigned integer
+            sbyte var2 = 2;//Signed integer	
+            int var3 = 3;//Signed integer
+            uint var4 = 4;//Unsigned integer
+            short var5 = 5;//Signed integer
+            ushort var6 = 6;//Unsigned integer
+            long var7 = 7;//Signed integer
+            ulong var8 = 8;//Unsigned integer
+            float var9 = 32;//Single-precision floating point type
+            double var10 = 12;//Double-precision floating point type
+            char var11 = 'A';
+            bool var12 = true;
+            string var13 = "Hello, World!";
+            decimal var14 =0.3m;
+            Console.WriteLine();
         }
     }
 }
